0716704da01addcb6d383de2cf7ef23682bec9e2
Bhaskar Gosiya <blgosiya622@gmail.com>
2018-04-13 10:27:35 +0530

    [IMP] Improved code

--- __openerp__.py
+++ __openerp__.py
@@ -1,9 +1,11 @@
 {
 	'name': 'Student Marksheet',
 	'version': '1.0.8.0',
+	'author': 'Bhaskar L. Gosiya',
 	'sequence': 5,
 	'depends':[],
 	'data':['views/student_view.xml',
+			'views/menu.xml',
 			],
 	'installable': True,
 	'auto_install': False,
--- models/student.py
+++ models/student.py
@@ -4,3 +4,11 @@ from openerp import fields, models
 class Student(models.Model):
 
 	_name = "student.student"
+
+	image = fields.Binary()
+	name = fields.Char(string="Student Name")
+	city = fields.Char(string="City Name")
+	email = fields.Char(string="E-Mail Address")
+	mobile = fields.Char(string="Mobile No.")
+	birthday = fields.Date(string="Birth Day")
+	age = fields.Integer(string="Age")
